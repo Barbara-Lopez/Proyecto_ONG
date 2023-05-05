b05afdd6040075af931ce7a5dddda4dd038ca8b7
Barbara <barbara.lopez@ikasle.egibide.org>
2023-05-05 21:48:06 +0200

    mejoras y añadir clase

---
+++ ONG/app/src/main/java/com/example/ong/RegistroNiebla.kt
@@ -0,0 +1,10 @@
+package com.example.ong
+
+class RegistroNiebla() {
+    var fecha:String = ""
+    var niebla:Boolean = false
+    var intensidadNiebla = ""
+    var franjaHoraria:String = ""
+    var duracionLluvia:String = "0"
+    var duracionCortesAgua:String = "0"
+}
